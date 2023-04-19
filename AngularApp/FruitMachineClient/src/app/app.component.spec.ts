4fd40b5c7e9ba8d9ca9206a3e3d75ccee490a382
leogorman <leogorman.contact@gmail.com>
2023-04-19 19:46:33 +0100

    Rename file, remove boilerplate html code, add code and template for displaying fields + allowing user to spin and reset

--- AngularApp/FruitMachineClient/src/app/app.component.spec.ts
+++ AngularApp/FruitMachineClient/src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FruitMachineClient'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('FruitMachineClient');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- AngularApp/FruitMachineClient/src/app/app.component.ts
+++ AngularApp/FruitMachineClient/src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { FruitMachineResult } from './fruit-machine.result';
+import { FruitMachineService } from './app.service';
+import { Color } from './fruit-machine.result';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <button [disabled]="buttonDisabled" (click)="spin()">Spin</button>
+    <button (click)="reset()">Reset</button>
+    <div *ngIf="result">
+      <div>Colors: {{ result.colors }}</div>
+      <div>Balance: {{ result.balance }}</div>
+      <div *ngIf="result.winner">Winner!</div>
+      <div *ngIf="!result.winner">Sorry, you lost.</div>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'FruitMachineClient';
+
+  result: FruitMachineResult | undefined;
+  buttonDisabled: boolean = false;
+
+  constructor(private fruitMachineService: FruitMachineService) {}
+
+  spin() {
+      this.fruitMachineService.spin().subscribe((result) => {
+        this.result = result;
+        if(result.balance == 0){
+            this.buttonDisabled = true;
+        } else{
+          this.buttonDisabled = false;
+        }
+      });
+  }
+
+  reset() {
+    this.fruitMachineService.reset().subscribe((result) => {
+      this.result = result;
+      this.buttonDisabled = false;
+    });
+  }
+
+  getColorName(color: Color) {
+    return Color[color];
+  }
 }
