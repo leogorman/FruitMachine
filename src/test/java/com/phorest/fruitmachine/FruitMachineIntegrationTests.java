8f6d8e792173680654a61c20c8534212c3e8300b
leogorman <leogorman.contact@gmail.com>
2023-04-19 18:30:02 +0100

    Write failing integration test to hit /fruit-machine

---
+++ src/test/java/com/phorest/fruitmachine/FruitMachineIntegrationTests.java
@@ -0,0 +1,30 @@
+package com.phorest.fruitmachine;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class FruitMachineIntegrationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testSpinEndpoint() {
+        ResponseEntity<FruitMachineResult> response = restTemplate.postForEntity("/api/fruit-machine", null, FruitMachineResult.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        FruitMachineResult result = response.getBody();
+        assertNotNull(result);
+    }
+}
+
+
