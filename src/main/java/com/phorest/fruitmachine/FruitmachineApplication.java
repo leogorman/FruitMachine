17dcbe4c6ea9844c4bb6c3a1f3ed8f9007fe6a11
leogorman <leogorman.contact@gmail.com>
2023-04-19 18:22:37 +0100

    Created initial Spring project using Spring Initializer

---
+++ src/main/java/com/phorest/fruitmachine/FruitmachineApplication.java
@@ -0,0 +1,13 @@
+package com.phorest.fruitmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitmachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitmachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phorest/fruitmachine/FruitmachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phorest.fruitmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitmachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
